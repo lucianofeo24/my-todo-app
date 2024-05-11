f10d0d58f3dae550cb5d799a9953b36906f0e849
lucianofeo <luciano.feo.bn@gmail.com>
2024-05-11 09:46:07 +0200

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = '/Users/lucianofeo/Desktop/Personale/Python Mega Course/ToDoList/todos.txt'
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file_local:
+        # 'todos_local' è una lista
+        todos_local = file_local.readlines()
+    return todos_local
+
+# la funzione non ritorna nulla perchè serve a scrivere su un file di testo e non deve restituire nulla al codice
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print(get_todos())
---
+++ web.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import functions as ft
+
+todos = ft.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    ft.write_todos(todos)
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+
+for i, v in enumerate(todos):
+    checkbox = st.checkbox(v, key=i)
+    if checkbox:
+        todos.pop(i)
+        ft.write_todos(todos)
+        del st.session_state[i]
+        st.experimental_rerun()
+
+st.text_input("scrivi", on_change=add_todo, key='new_todo')
